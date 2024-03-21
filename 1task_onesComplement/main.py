fa685f7aea8326b6b202d50e7e5ce473c814d1f1
Михаил Грудинин <76101411+touge13@users.noreply.github.com>
2024-03-21 10:56:28 +0300

    Add files via upload

--- 1task_onesComplement/main.py
+++ 1task_onesComplement/main.py
@@ -1,20 +1,11 @@
 from arithmetic import Binary
-from tests import tests
+from visualization import visualization
 
-num1 = Binary(int(input("Введите первое число: ")))
-num2 = Binary(int(input("Введите второе число: ")))
-binary_num1 = num1.to_binary()
-binary_num2 =num2.to_binary()
-
-print(f"Первое число в двоичном коде: {binary_num1}")
-print(f"Второе число в двоичном коде: {binary_num2}")
-
-print(f"Сумма чисел в двоичном коде: {num1 + num2}, в десятичной системе счисления: {num1.to_decimal(binary_num1) + num2.to_decimal(binary_num2)}")
-print(f"Разность чисел в двоичном коде: {num1 - num2}, в десятичной системе счисления: {num1.to_decimal(binary_num1) - num2.to_decimal(binary_num2)}")
-print(f"Произведение чисел в двоичном коде: {num1 * num2}, в десятичной системе счисления: {num1.to_decimal(binary_num1) * num2.to_decimal(binary_num2)}")
-print(f"Частное чисел в двоичном коде: {num1 / num2}, в десятичной системе счисления: {num1.to_decimal(binary_num1) // num2.to_decimal(binary_num2)}")
+input1 = int(input("Введите первое число: "))
+input2 = int(input("Введите второе число: "))
+visualization(input1, input2)
 
 #выведем пару тестов
-tests(6, 8)
-tests(0, -10)
-tests(-14, -7)
+visualization(6, 8)
+visualization(0, -10)
+visualization(-14, -7)
